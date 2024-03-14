dfb798b6719bbece0141e4c7483f96b2dc05ec02
maryRom01 <maryna_romanets@aol.com>
2024-03-14 16:55:15 -0400

    first attempt

---
+++ brackets2/src/src/Main.java
@@ -0,0 +1,59 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static boolean compile(String input) {
+        int openCurly = 0;
+        int openSquare = 0;
+        int openDiamond = 0;
+        int openRound = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            switch (c) {
+                case '{': openCurly++; break;
+                case '}': if (openCurly < 1) return false; openCurly--; break;
+                case '[': openSquare++; break;
+                case ']': if (openSquare < 1) return false; openSquare--; break;
+                case '<': openDiamond++; break;
+                case '>': if (openDiamond < 1) return false; openDiamond--; break;
+                case '(': openRound++; break;
+                case ')': if (openRound < 1) return false; openRound--; break;
+                default:
+            }
+        }
+        return (openCurly == 0 & openSquare == 0 & openDiamond == 0 & openRound == 0);
+    }
+
+    // [] () {} <>
+    // [({})<>{}]
+    public static void main(String[] args) {
+        String inputValid = """
+                public void soSomething(double x) {
+                  [({})<>{}]
+                }
+                """;
+        String inputInvalid = """
+                public void soSomething(double x) {
+                  [({})><{}]
+                }
+                    """;
+        String inputInvalid2 = """
+                public void soSomething(double x) {
+                  [(}{)><{}]
+                }
+                    """;
+        String inputInvalid3 = """
+                public void soSomething(double x) {
+                  [({<{
+                }
+                    """;
+        boolean result1 = compile(inputValid);  // true
+        boolean result2 = compile(inputInvalid);// false
+        boolean result3 = compile(inputInvalid2);// false
+        boolean result4 = compile(inputInvalid3);// false
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
